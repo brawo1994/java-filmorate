adf2372e8f5195c8541761af3438fe25028a4d8f
Александр <KokotovAlexandr@yandex.ru>
2022-12-25 10:46:53 +0300

    fix: исправить ошибки слияния веток.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/impl/FilmDbStorage.java
@@ -32,22 +32,22 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public List<Film> getRecommendations(int id) {
         return jdbcTemplate.query("SELECT DISTINCT F.ID," +
-                                                  "NAME, " +
-                                                  "DESCRIPTION," +
-                                                  "RELEASE_DATE," +
-                                                  "DURATION, " +
-                                                  "RATING_MPA " +
-                                      " FROM FILMS F " +
-                                      " JOIN FILMS_LIKE FL ON F.ID = FL.FILM_ID " +
-                                      "WHERE FL.USER_ID IN (" +
-                                            "SELECT DISTINCT F2.USER_ID AS USER_COMMON_FILMS " +
-                                            "FROM FILMS_LIKE F " +
-                                            "JOIN FILMS_LIKE FL ON F.FILM_ID = FL.FILM_ID " +
-                                            "JOIN FILMS_LIKE F2 ON F2.FILM_ID = FL.FILM_ID " +
-                                            "WHERE FL.USER_ID = ?) " +
-                                      "  AND F.ID NOT IN (" +
-                                            "SELECT FILM_ID FROM FILMS_LIKE" +
-                                            "WHERE USER_ID = ?)", this::makeFilm, id, id);
+                "NAME, " +
+                "DESCRIPTION," +
+                "RELEASE_DATE," +
+                "DURATION, " +
+                "RATING_MPA " +
+                " FROM FILMS F " +
+                " JOIN FILMS_LIKE FL ON F.ID = FL.FILM_ID " +
+                "WHERE FL.USER_ID IN (" +
+                "SELECT DISTINCT F2.USER_ID AS USER_COMMON_FILMS " +
+                "FROM FILMS_LIKE F " +
+                "JOIN FILMS_LIKE FL ON F.FILM_ID = FL.FILM_ID " +
+                "JOIN FILMS_LIKE F2 ON F2.FILM_ID = FL.FILM_ID " +
+                "WHERE FL.USER_ID = ?) " +
+                "  AND F.ID NOT IN (" +
+                "SELECT FILM_ID FROM FILMS_LIKE" +
+                "WHERE USER_ID = ?)", this::makeFilm, id, id);
     }
 
     @Override
@@ -60,15 +60,15 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public List<Film> getCommonFilms(int userId, int friendId) {
         String sqlQuery = "SELECT DISTINCT f.id, " +
-                                          "f.name, " +
-                                          "f.description, " +
-                                          "f.release_date, " +
-                                          "f.duration, " +
-                                          "f.rating_mpa " +
-                          "FROM films f " +
-                          "INNER JOIN films_like fl ON f.id = fl.film_id " +
-                          "INNER JOIN films_like f2 ON f2.film_id = fl.film_id " +
-                          "WHERE fl.user_id = ? AND f2.user_id = ?";
+                "f.name, " +
+                "f.description, " +
+                "f.release_date, " +
+                "f.duration, " +
+                "f.rating_mpa " +
+                "FROM films f " +
+                "INNER JOIN films_like fl ON f.id = fl.film_id " +
+                "INNER JOIN films_like f2 ON f2.film_id = fl.film_id " +
+                "WHERE fl.user_id = ? AND f2.user_id = ?";
 
         return jdbcTemplate.query(sqlQuery, this::makeFilm, userId, friendId);
     }
@@ -84,7 +84,7 @@ public class FilmDbStorage implements FilmStorage {
                 "FROM films AS f " +
                 "LEFT JOIN films_genre AS fg ON f.id = fg.film_id " +
                 "LEFT JOIN films_like AS fl ON f.id = fl.film_id " +
-                 condition +
+                condition +
                 "GROUP BY f.id, f.name,f.description, f.release_date, f.duration, f.rating_mpa " +
                 "ORDER BY COUNT(fl.user_id) DESC " +
                 "LIMIT ?";
